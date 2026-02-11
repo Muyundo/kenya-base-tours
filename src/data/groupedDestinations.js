f81b235014cbe6aee3ea6a22cd9269dd1fccda72
MUYUNDO BRIAN <muyundobrian@gmail.com>
2026-02-12 00:12:37 +0300

    introducing groupedDestinations.js

---
+++ src/data/groupedDestinations.js
@@ -0,0 +1,35 @@
+export const groupedDestinations = [
+  {
+    group: "Rift Valley",
+    destinations: [
+      { name: "Lake Nakuru", route: "/destinations/LakeNakuru" },
+      { name: "Masai Mara", route: "/destinations/MasaiMara" },
+      { name: "Rift Valley", route: "/destinations/RiftValley" },
+      { name: "Lake Elementaita", route: "/destinations/LakeElementaita" }
+    ]
+  },
+  {
+    group: "Nanyuki",
+    destinations: [
+      { name: "Mt Kenya", route: "/destinations/MountKenya" },
+      { name: "Mt Kilimanjaro View", route: "/destinations/MtKilimanjaroView" },
+      { name: "Samburu", route: "/destinations/Samburu" }
+    ]
+  },
+  {
+    group: "Amboseli",
+    destinations: [
+      { name: "Amboseli", route: "/destinations/Amboseli" },
+      { name: "Tsavo East and West", route: "/destinations/Tsavo" }
+    ]
+  },
+  {
+    group: "Coast",
+    destinations: [
+      { name: "Lamu", route: "/destinations/Lamu" },
+      { name: "Hotels", route: "/destinations/Hotels" },
+      { name: "Diani South Coast Hotels", route: "/destinations/Diani" },
+      { name: "North Coast", route: "/destinations/NorthCoast" }
+    ]
+  }
+];
